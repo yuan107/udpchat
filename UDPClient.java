b0f2eb978954285334691c1ad8ef7644ad435dd9
Michael <michaelyuan94@gmail.com>
2015-09-22 02:36:46 -0700

    Client working

--- UDPClient.java
+++ UDPClient.java
@@ -12,33 +12,82 @@ import java.io.*;
 import java.net.*;
 
 class UDPClient {
-	
-    public static void main(String args[]) throws Exception
-    {
-		while(true){
-      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+  static DatagramSocket clientSocket = null;
+  static String clientColor = "";
+  static String otherClientColor = "";
+  static InetAddress IPAddress = null;
 
-      DatagramSocket clientSocket = new DatagramSocket();
+  public static void main(String args[]) throws Exception{
+    clientSocket = new DatagramSocket();
+    //IPAddress = InetAddress.getByName("10.49.139.107");
+    IPAddress = InetAddress.getByName("192.168.0.9");
+    BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Waiting to connect to server...\n");
+    sendString("500");//the I want to conncect code
+    try{
+      Runtime.getRuntime().addShutdownHook(new Thread(){
+        public void run(){
+      try{
+      sendString("300");
+    } catch (Exception e){
 
-      InetAddress IPAddress = InetAddress.getByName("10.49.139.107");
+    }
 
-      byte[] sendData = new byte[1024];
-      byte[] receiveData = new byte[1024];
-
-      String sentence = inFromUser.readLine();
-      sendData = sentence.getBytes();
-	  DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-
-      clientSocket.send(sendPacket);
-
-      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
-      clientSocket.receive(receivePacket);
-
-      String modifiedSentence = new String(receivePacket.getData());
-
-      System.out.println("FROM SERVER:" + modifiedSentence);
       clientSocket.close();
-		}
+        }
+      });
+      while(true){
+
+        // byte[] sendData = new byte[1024];
+
+        // String sentence = inFromUser.readLine();
+        // sendData = sentence.getBytes();
+        // DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+
+        // clientSocket.send(sendPacket);
+
+        String modifiedSentence = recieveString();
+        if(modifiedSentence.substring(0,3).equals("100")){//I am red
+          clientColor = "Red";
+          otherClientColor = "Blue";
+          String blueConnectedMessage = "404";
+          System.out.println("\nWaiting for Blue to connect...");
+          while (!blueConnectedMessage.substring(0,3).equals("200")){
+            blueConnectedMessage = recieveString();
+          }
+          System.out.println("\n\n---------------------------------------\n");
+          modifiedSentence = "200 Blue connected! Type the first message!\n";
+        } else if(modifiedSentence.substring(0,3).equals("200")){//I am blue
+          clientColor = "Blue";
+          otherClientColor = "Red";
+          System.out.println("\nConnected! Waiting for Red to send the first message...\n");
+          modifiedSentence = recieveString();//wait for message from red
+        }
+
+        System.out.println(otherClientColor + ": " + modifiedSentence.substring(3) + "\n" + clientColor + ":");
+
+        sendString("000" + inFromUser.readLine());
       }
+    }catch(Exception e){
+      clientSocket.close();
+      sendString("300");
+    }
+  }
+
+  public static String recieveString() throws IOException{
+    byte[] receiveData = new byte[1024];
+    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+    clientSocket.receive(receivePacket);
+    return new String(receivePacket.getData()).trim();
+  }
+
+  public static void sendString(String str) throws IOException{
+    if(clientSocket == null){
+      clientSocket = new DatagramSocket();
+    }
+    byte[] sendData = new byte[1024];
+    sendData = str.getBytes();
+    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+    clientSocket.send(sendPacket);
+  }
 }
--- UDPServer.java
+++ UDPServer.java
@@ -17,71 +17,82 @@ class UDPServer {
 	public static InetAddress RedIP = null;
 	public static InetAddress BlueIP = null;
 	
-  public static void main(String args[]) throws Exception
-    {
+	public static void main(String args[]) throws Exception
+	{
     //DatagramSocket serverSocket = null;
 	//int port = 0;
-	  
-	try
+
+		try
 		{
 			serverSocket = new DatagramSocket(9876);
 		}
-	
-	catch(Exception e)
+
+		catch(Exception e)
 		{
 			System.out.println("Failed to open UDP socket");
 			System.exit(0);
 		}
 
-      while(true)
-        {
-      byte[] receiveData = new byte[1024];
-      byte[] sendData  = new byte[1024];
-          DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		  
-          serverSocket.receive(receivePacket);
-		  
-          String sentence = new String(receivePacket.getData());
+		System.out.println("\n\nServer Open. Listening for Clients...");
 
-          InetAddress IPAddress = receivePacket.getAddress();
+		while(true)
+		{
+			byte[] receiveData = new byte[1024];
+			byte[] sendData  = new byte[1024];
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-          port = receivePacket.getPort();
+			serverSocket.receive(receivePacket);
 
-          sendData = sentence.getBytes();
-		  
-		  System.out.println("MSG RECIEVED: " + sentence);
-		  
-		  InetAddress sendAddress = null;
-		  if(IPAddress.equals(RedIP) && BlueIP != null){
-			  sendAddress = BlueIP;
-			  System.out.println("Sending to Blue");
-		  } else if(IPAddress.equals(BlueIP) && RedIP != null) {
-			  sendAddress = RedIP;
-			  System.out.println("Sending to Red");
-		  } else if(RedIP == null){
-			  RedIP = IPAddress;
-			  sendAddress = RedIP;
-			  sendData = new String("100: You are Red").getBytes();
-		  } else if(BlueIP == null){
-			  BlueIP = IPAddress;
-			  sendAddress = BlueIP;
-			  sendData = new String("200: You are Blue").getBytes();
-			 
-			//send to both IP's
-			DatagramPacket sendPacket2 = new DatagramPacket(sendData, sendData.length, RedIP, port);
-			serverSocket.send(sendPacket2);
-		  } else{
-			  BlueIP = IPAddress;
-			  sendAddress = BlueIP;
-			  sendData = new String("404: Something screwed up").getBytes();
-			 
-			//send to both IP's
-			DatagramPacket sendPacket2 = new DatagramPacket(sendData, sendData.length, RedIP, port);
-			serverSocket.send(sendPacket2);
-		   }
-          DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, sendAddress, port);
+			String sentence = new String(receivePacket.getData());
 
-          serverSocket.send(sendPacket);
-        }
-    }
+			InetAddress IPAddress = receivePacket.getAddress();
+
+			port = receivePacket.getPort();
+
+			sendData = sentence.getBytes();
+
+			System.out.println("MSG RECIEVED: " + sentence + "|END OF MESSAGE|MessageCode:" + sentence.substring(0,3) + "\n");
+
+			if(sentence.substring(0,3).equals("300") && IPAddress.equals(RedIP)){//Disconnect Message
+				RedIP = null;
+				System.out.println("Red Disconnected");
+			}
+			if(sentence.substring(0,3).equals("300") && IPAddress.equals(BlueIP)){//Disconnect Message
+				BlueIP = null;
+				System.out.println("Blue Disconnected");
+			}
+
+			InetAddress sendAddress = null;
+			if(IPAddress.equals(RedIP) && BlueIP != null){
+				sendAddress = BlueIP;
+				System.out.println("Sending to Blue");
+				sendData = sentence.getBytes();
+			} else if(IPAddress.equals(BlueIP) && RedIP != null) {
+				sendAddress = RedIP;
+				System.out.println("Sending to Red");
+				sendData = sentence.getBytes();
+			} else if(RedIP == null){
+				RedIP = IPAddress;
+				sendAddress = RedIP;
+				sendData = new String("100 Red Client Connected").getBytes();
+			} else if(BlueIP == null && !RedIP.equals(IPAddress)){
+				BlueIP = IPAddress;
+				sendAddress = BlueIP;
+				sendData = new String("200 Blue Client Connected").getBytes();
+
+			    //send to both IP's
+				DatagramPacket sendPacket2 = new DatagramPacket(sendData, sendData.length, RedIP, port);
+				serverSocket.send(sendPacket2);
+			} else{
+				BlueIP = IPAddress;
+				sendAddress = BlueIP;
+				sendData = new String("404 Client Not Found").getBytes();
+			    //send to both IP's
+				DatagramPacket sendPacket2 = new DatagramPacket(sendData, sendData.length, RedIP, port);
+				serverSocket.send(sendPacket2);
+			}
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, sendAddress, port);
+			serverSocket.send(sendPacket);
+		}
+	}
 }
